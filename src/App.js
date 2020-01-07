e43a33574b0638725a6618b7f05941a0bfc8547d
danWagar <dandwagar@gmail.com>
2020-01-07 11:55:35 -0800

    resolve issues with merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
-=======
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Header from './components/Header/Header';
 // import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
->>>>>>> e0eb17d7bb4b74edfb1bb9e85ac1efebc51164b6
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -18,14 +13,11 @@ function App() {
       </header>
       <main className="app__main">
         <Switch>
-<<<<<<< HEAD
           <Route exact path={'/'} component={Dashboard} />
-=======
           {/* routes go here */}
           {/* <Route exact path="/login" component={Login} /> */}
           <Route exact path="/register" component={Register} />
           {/* <Route exact path="/logout" render={() => firebase.logout}/> */}
->>>>>>> e0eb17d7bb4b74edfb1bb9e85ac1efebc51164b6
         </Switch>
       </main>
     </>

3f2e471106f71d4958fcee92b3cf33abf4bfd736
balayaydemir <balayaydemir@gmail.com>
2020-01-07 11:48:34 -0800

    job component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import Jobs from './components/Jobs/Jobs';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <Header />
       </header>
       <main className="app__main">
+        <Jobs />
         <Switch>
           {/* routes go here */}
         </Switch>
---
+++ src/components/Jobs/Jobs.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { ProgressBar } from '../ProgressBar/ProgressBar';
+
+export default class Jobs extends Component {
+  static defaultProps = {
+    job: {},
+    userRole: ''
+  }
+
+  state = {
+    expandJob: false,
+    job: 
+      {
+        jobId: 1,
+        jobName: 'Rock Faces',
+        jobDetails: 'Sing really well',
+        jobProgress: 50,
+        jobApproval: false,
+        jobRevision: false,
+        jobEmployees: ['Brennan Huff', 'Dr. Doback']
+      }
+    ,
+    userRole: 'employee'
+  }
+
+  renderEmployeeList = (employees) => {
+    return employees.map((employee, index) => {
+      let itemKey = index+employee
+      return <li key={itemKey}>
+        {employee}
+      </li>
+    })
+  }
+
+  renderProjectButtons(approval, progress) {
+    if (this.state.userRole === 'employee') {
+      if (approval || progress !== 100) {
+        return <button disabled>Submit for Approval</button>
+      } else {
+        return <button>Submit for Approval</button>
+      }
+    }
+    if (this.state.userRole === 'project manager' || this.state.userRole === 'owner') {
+      return <div className="manager__buttons">
+        <span>{!approval && progress === 100 ? 'AWAITING APPROVAL' : ''}</span>
+        <button>Assign</button>
+        <button>Edit</button>
+      </div>
+    }
+  }
+
+  toggleExpand = () => {
+    this.setState({
+      expandJob: !this.state.expandJob
+    })
+  }
+
+  render() {
+    let job = this.state.job
+    return (
+      <>
+         <li key={job.jobId} id={job.jobId}>
+            <button onClick={this.toggleExpand}>{this.state.expandJob ? '-' : '+'}</button>
+            <h4>{job.jobName}</h4>
+            <span>{job.jobDetails}</span>
+            <div className="job-progress">
+              <ProgressBar percentage={job.jobProgress} />
+            </div>
+            {this.renderProjectButtons(job.jobApproval, job.jobProgress)}
+            {this.state.expandJob ? <ul>{this.renderEmployeeList(job.jobEmployees)}</ul> : ''}
+          </li>
+      </>
+    )
+  }
+}

0e197b381df24824afd9a2c6e2eccd065aef2abe
danWagar <dandwagar@gmail.com>
2020-01-12 16:33:58 -0800

    auth working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Header from "./components/Header/Header";
-import { Login, Logout, Register } from "./components/Account/index";
-import NewProject from "./components/NewProject/NewProject";
-import ProjectView from "./components/ProjectView/ProjectView";
-import LandingPage from "./routes/LandingPage/LandingPage";
+import React, { useContext } from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Header from './components/Header/Header';
+import Login from './components/Account/Login';
+
+import SignUp from './components/Account/SignUp';
+import NewProject from './components/NewProject/NewProject';
+import ProjectView from './components/ProjectView/ProjectView';
+import LandingPage from './routes/LandingPage/LandingPage';
+import PrivateRoute from './services/PrivateRoute';
+import { AuthContext } from './services/Auth.js';
 // import NewJob from "./components/NewJob/NewJob";
 
 // import { auth } from "./services/firebase";
@@ -13,37 +17,30 @@ import LandingPage from "./routes/LandingPage/LandingPage";
 // import db from "./services/firebase";
 
 const App = props => {
+  const { currentUser } = useContext(AuthContext);
+  console.log(currentUser);
   return (
-    <BrowserRouter>
+    <Router>
       <header>
         <Header />
       </header>
       <main className="app__main">
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute exact path="/dashboard" component={() => <Dashboard user={currentUser} />} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/register" component={SignUp} />
 
-          <Route
-            exact
-            path="/new_project"
-            render={props => <NewProject {...props} />}
-          />
+          <Route exact path="/new_project" render={props => <NewProject {...props} />} />
           {/* <Route
             exact
             path="/new_job"
             render={props => <NewJob {...props} />}
           /> */}
-          <Route
-            exact
-            path="/project/:id"
-            component={props => <ProjectView id={props.match.params.id} />}
-          />
+          <Route exact path="/project/:id" component={props => <ProjectView id={props.match.params.id} />} />
         </Switch>
       </main>
-    </BrowserRouter>
+    </Router>
   );
 };
 

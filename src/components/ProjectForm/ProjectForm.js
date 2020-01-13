3722d280d35a6718b7776cc117ae5bad6a1ad32b
Alex Bannow <rbannal@gmail.com>
2020-01-13 10:21:11 -0500

    working job/project ids and adds

--- src/components/ProjectForm/ProjectForm.js
+++ src/components/ProjectForm/ProjectForm.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Input, Label } from '../Form/Form';
-import FirebaseContext from '../../services/context';
-import * as firebase from 'firebase/app';
+import React, { Component } from "react";
+import { Input, Label } from "../Form/Form";
+import FirebaseContext from "../../services/context";
+import * as firebase from "firebase/app";
 
 export default class ProjectForm extends Component {
   static contextType = FirebaseContext;
@@ -18,21 +18,38 @@ export default class ProjectForm extends Component {
       date_created: new Date(),
       org_id: this.context.user.org,
       progress: 0,
-      project_workers: []
+      project_workers: [],
+      id: null
     };
     await this.context.addProject(data);
-    this.props.history.push('/dashboard');
+    this.props.history.push("/dashboard");
   };
 
   render() {
     return (
       <form className="ProjectForm" onSubmit={e => this.handleSubmit(e)}>
         <Label htmlFor="project_name">Name</Label>
-        <Input name="name" id="project_name" type="text" placeholder="Project Name" required />
+        <Input
+          name="name"
+          id="project_name"
+          type="text"
+          placeholder="Project Name"
+          required
+        />
         <Label htmlFor="project_description">Description</Label>
-        <Input name="description" id="project_description" type="text" placeholder="Project Description" />
+        <Input
+          name="description"
+          id="project_description"
+          type="text"
+          placeholder="Project Description"
+        />
         <Label htmlFor="project_description">Project Manager</Label>
-        <Input name="projectManager" id="project_manager" type="text" placeholder="Project Manager" />
+        <Input
+          name="projectManager"
+          id="project_manager"
+          type="text"
+          placeholder="Project Manager"
+        />
         <Label htmlFor="project_deadline">Deadline</Label>
         <input name="deadline" id="project_deadline" type="date" />
         <button type="submit">SUBMIT</button>

5a94b1ad9232cae5ddb0864245d664e528b39c88
balayaydemir <balayaydemir@gmail.com>
2020-01-13 11:05:21 -0800

    cleanup

--- src/components/Account/Logout.js
+++ src/components/Account/Logout.js
@@ -2,8 +2,8 @@ import React from 'react';
 import app from '../../services/base.js';
 import { Redirect } from 'react-router-dom';
 
-const Logout = props => {
-  app.auth().signOut();
+const Logout = async props => {
+  await app.auth().signOut();
   return <Redirect to="/" />;
 };
 

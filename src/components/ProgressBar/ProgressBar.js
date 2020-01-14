5783e3f4e2b252371de4677270129b2057d53b46
balayaydemir <balayaydemir@gmail.com>
2020-01-14 12:23:19 -0800

    edit job working

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,12 +3,12 @@ import "./ProgressBar.css";
 
 export const ProgressBar = props => {
   return (
-    <div className="ProgressBar__meter">
-      <span
-        className="ProgressBar__fill"
-        style={{ width: props.percentage + "%" }}
-      ></span>
-      <span className="ProgressBar__text">{props.percentage}%</span>
-    </div>
+      <div className="ProgressBar__meter">
+        <span
+          className="ProgressBar__fill"
+          style={{ width: props.percentage + "%" }}
+        ></span>
+        <span className="ProgressBar__text">{props.percentage}%</span>
+      </div>
   );
 };

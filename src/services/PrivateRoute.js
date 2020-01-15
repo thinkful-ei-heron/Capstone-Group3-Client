9e758d187b6631de096b8d8a485562c985d647f9
reif <bridgerrhammond@gmail.com>
2020-01-15 07:08:40 -0600

    prettier

--- src/services/PrivateRoute.js
+++ src/services/PrivateRoute.js
@@ -1,16 +1,22 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { AuthContext } from './Auth';
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { AuthContext } from "./Auth";
 
 const PrivateRoute = ({ component: RouteComponent, location, ...rest }) => {
   const { currentUser } = useContext(AuthContext);
 
-  if (currentUser) localStorage.setItem('path', location.pathname);
+  if (currentUser) localStorage.setItem("path", location.pathname);
 
   return (
     <Route
       {...rest}
-      render={routeProps => (!!currentUser ? <RouteComponent {...routeProps} /> : <Redirect to={'/'} />)}
+      render={routeProps =>
+        !!currentUser ? (
+          <RouteComponent {...routeProps} />
+        ) : (
+          <Redirect to={"/"} />
+        )
+      }
     />
   );
 };

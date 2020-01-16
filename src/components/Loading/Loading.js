2b5ac8060523a4cd81a3f52378e6eb39b1109814
danWagar <dandwagar@gmail.com>
2020-01-16 13:55:48 -0800

    loading screen styling, changes to dashboard and initDashboard in dbServices

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactLoading from "react-loading";
+import React from 'react';
+import ReactLoading from 'react-loading';
+import './Loading.css';
 
 export default function Loading(props) {
+  //{props.className}
   return (
-    <div className={props.className}>
-      <ReactLoading type={"bars"} color={"#3b59ff"} />
+    <div className="Loading">
+      <ReactLoading type={'spokes'} color={'#3b59ff'} height={125} width={125} />
     </div>
   );
 }

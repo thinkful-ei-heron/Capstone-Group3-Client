dd3b552b7c678091588d9ff9f5bbbcd397a39230
balayaydemir <balayaydemir@gmail.com>
2020-01-17 08:02:50 -0800

    workereditform

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-//import FirebaseContext from '../../services/context.js';
 import { AuthContext } from '../../services/Auth';
 import Loading from '../Loading/Loading';
 import app from '../../services/base';
 import './Jobs.css';
-import dbServices from '../../services/dbServices';
 import JobItem from './JobItem'
 
 export default class Jobs extends Component {

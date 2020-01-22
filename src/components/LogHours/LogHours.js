dc7419b09ec77f58c950283a71bcf34cb3d49ce1
reif <bridgerrhammond@gmail.com>
2020-01-22 14:27:19 -0600

    fixed whatever the problem was

--- src/components/LogHours/LogHours.js
+++ src/components/LogHours/LogHours.js
@@ -12,7 +12,6 @@ const LogHours = props => {
   const { value: job, bind: bindJob, reset: resetJob } = useInput("");
   const [submitted, setSubmitted] = useState(false);
 
-
   const populateSelect = () => {
     return props.jobs.map(job => {
       if (job.project_workers.includes(currentUser.name))
@@ -42,12 +41,16 @@ const LogHours = props => {
     let jobObj = props.jobs.find(item => item.name === job);
     let oldHours = parseInt(jobObj.hours_completed);
     let newHours = oldHours + parseInt(hours);
-    let employeeHoursObj = jobObj.employee_hours.find(item => item.name === currentUser.name)
-    let oldEmpHours = parseInt(employeeHoursObj.hours)
-    let newEmpHours = oldEmpHours + parseInt(hours)
-    employeeHoursObj.hours = newEmpHours
-    let index = jobObj.employee_hours.findIndex(item => item.name === employeeHoursObj.name)
-    jobObj.employee_hours[index] = employeeHoursObj
+    let employeeHoursObj = jobObj.employee_hours.find(
+      item => item.name === currentUser.name
+    );
+    let oldEmpHours = parseInt(employeeHoursObj.hours);
+    let newEmpHours = oldEmpHours + parseInt(hours);
+    employeeHoursObj.hours = newEmpHours;
+    let index = jobObj.employee_hours.findIndex(
+      item => item.name === employeeHoursObj.name
+    );
+    jobObj.employee_hours[index] = employeeHoursObj;
     jobObj.hours_completed = newHours;
 
     dbServices

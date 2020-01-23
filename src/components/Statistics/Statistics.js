4e1727b20cb959f5e51ae410a833383a4a16afd7
reif <bridgerrhammond@gmail.com>
2020-01-23 10:15:25 -0600

    stats2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import { Bar, Line, Pie } from 'react-chartjs-2'
 import { AuthContext } from '../../services/Auth'
@@ -8,17 +7,6 @@ const db = app.firestore()
 
 export default class Statistics extends Component {
   static contextType = AuthContext
-=======
-import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
-import { AuthContext } from "../../services/Auth";
-import app from "../../services/base";
-import './Statistics.css'
-const db = app.firestore();
-
-export default class Statistics extends Component {
-  static contextType = AuthContext;
->>>>>>> origin/chartjs
   state = {
     jobDue: {
       labels: [
@@ -42,7 +30,6 @@ export default class Statistics extends Component {
         ).getDate()}/${new Date(Date.now() + 86400000 * 5).getFullYear()}`,
         `${new Date(Date.now() + 86400000 * 6).getMonth() + 1}/${new Date(
           Date.now() + 86400000 * 6
-<<<<<<< HEAD
         ).getDate()}/${new Date(Date.now() + 86400000 * 6).getFullYear()}`,
       ],
       datasets: [
@@ -60,25 +47,6 @@ export default class Statistics extends Component {
           ],
         },
       ],
-=======
-        ).getDate()}/${new Date(Date.now() + 86400000 * 6).getFullYear()}`
-      ],
-      datasets: [
-        {
-          label: `Job's Due`,
-          data: [5, 1, 2, 0, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)",
-            "rgba(75, 192, 192, 0.6)",
-            "rgba(153, 102, 255, 0.6)",
-            "rgba(255, 159, 64, 0.6)",
-            "rgba(255, 99, 132, 0.6)"
-          ]
-        }
-      ]
->>>>>>> origin/chartjs
     },
     jobHistory: {
       labels: [
@@ -102,7 +70,6 @@ export default class Statistics extends Component {
         ).getDate()}/${new Date(Date.now() - 86400000).getFullYear()}`,
         `${new Date(Date.now() - 86400000).getMonth() + 1}/${new Date(
           Date.now() - 86400000
-<<<<<<< HEAD
         ).getDate()}/${new Date(Date.now() - 86400000).getFullYear()}`,
       ],
       datasets: [
@@ -137,63 +104,6 @@ export default class Statistics extends Component {
         .then(snapshot => {
           let jobDueCount = 0
           let jobHistoryCount = 0
-=======
-        ).getDate()}/${new Date(Date.now() - 86400000).getFullYear()}`
-      ],
-      datasets: [
-        {
-          label: `Job Completion History (previous week)`,
-          data: [0, 1, 2, 1, 4],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.6)",
-            "rgba(54, 162, 235, 0.6)",
-            "rgba(255, 206, 86, 0.6)",
-            "rgba(75, 192, 192, 0.6)",
-            "rgba(153, 102, 255, 0.6)",
-            "rgba(255, 159, 64, 0.6)",
-            "rgba(255, 99, 132, 0.6)"
-          ]
-        }
-      ]
-    }
-    // employeeHours: {
-    //   labels: ["Reif", "Balay", "Dan", "Alex", "Adam"],
-    //   datasets: [
-    //     {
-    //       label: `Employee Job Hours`,
-    //       data: [8, 8, 7, 6, 3],
-    //       backgroundColor: [
-    //         "rgba(255, 99, 132, 0.6)",
-    //         "rgba(54, 162, 235, 0.6)",
-    //         "rgba(255, 206, 86, 0.6)",
-    //         "rgba(75, 192, 192, 0.6)",
-    //         "rgba(153, 102, 255, 0.6)",
-    //         "rgba(255, 159, 64, 0.6)",
-    //         "rgba(255, 99, 132, 0.6)"
-    //       ]
-    //     }
-    //   ]
-    // },
-    // jobs: []
-  };
-
-  componentDidMount = async () => {
-    const jobDue = [];
-    const jobHistory = [];
-    const employeeLabels = [];
-    const employeeHours = [];
-    for (let i = 0; i < this.state.jobDue.labels.length; i++) {
-      await db
-        .collection("organizations")
-        .doc(this.context.currentUser.org)
-        .collection("projects")
-        .doc(this.props.id)
-        .collection("jobs")
-        .get()
-        .then(snapshot => {
-          let jobDueCount = 0;
-          let jobHistoryCount = 0;
->>>>>>> origin/chartjs
           snapshot.docs.forEach(doc => {
             if (
               this.state.jobDue.labels[i] ===
@@ -204,11 +114,7 @@ export default class Statistics extends Component {
                 doc.data().deadline.seconds * 1000
               ).getFullYear()}`
             ) {
-<<<<<<< HEAD
               jobDueCount++
-=======
-              jobDueCount++;
->>>>>>> origin/chartjs
             }
             if (doc.data().date_completed) {
               if (
@@ -220,7 +126,6 @@ export default class Statistics extends Component {
                   ).getDate()}/${new Date(
                     doc.data().date_completed.seconds * 1000
                   ).getFullYear()}` &&
-<<<<<<< HEAD
                 doc.data().status === 'completed'
               ) {
                 jobHistoryCount++
@@ -239,18 +144,6 @@ export default class Statistics extends Component {
     }
     console.log(jobDue)
     console.log(jobHistory)
-=======
-                doc.data().status === "completed"
-              ) {
-                jobHistoryCount++;
-              }
-            }
-          });
-          jobDue.push(jobDueCount);
-          jobHistory.push(jobHistoryCount);
-        });
-    }
->>>>>>> origin/chartjs
     this.setState({
       jobDue: {
         labels: this.state.jobDue.labels,
@@ -258,15 +151,9 @@ export default class Statistics extends Component {
           {
             label: this.state.jobDue.datasets[0].label,
             data: jobDue,
-<<<<<<< HEAD
             backgroundColor: this.state.jobDue.datasets[0].backgroundColor,
           },
         ],
-=======
-            backgroundColor: this.state.jobDue.datasets[0].backgroundColor
-          }
-        ]
->>>>>>> origin/chartjs
       },
       jobHistory: {
         labels: this.state.jobHistory.labels,
@@ -274,38 +161,17 @@ export default class Statistics extends Component {
           {
             label: this.state.jobHistory.datasets[0].label,
             data: jobHistory,
-<<<<<<< HEAD
             backgroundColor: this.state.jobHistory.datasets[0].backgroundColor,
           },
         ],
       },
     })
   }
-=======
-            backgroundColor: this.state.jobHistory.datasets[0].backgroundColor
-          }
-        ]
-      },
-      // employeeHours: {
-      //   labels: employeeLabels,
-      //   datasets: [
-      //     {
-      //       label: this.state.employeeHours.datasets[0].label,
-      //       data: employeeHours,
-      //       backgroundColor: this.state.employeeHours.datasets[0]
-      //         .backgroundColor
-      //     }
-      //   ]
-      // }
-    });
-  };
->>>>>>> origin/chartjs
 
   render() {
     return (
       <div className="Statistics">
         <h5>STATISTICS</h5>
-<<<<<<< HEAD
 
         {this.state.jobDue.datasets[0].data.length !== 0 ? (
           <Bar
@@ -325,12 +191,6 @@ export default class Statistics extends Component {
         ) : (
           <span>You have not completed any tasks in the last 7 days</span>
         )}
-=======
-        <Bar className="due" data={this.state.jobDue} />
-        <Bar className="history" data={this.state.jobHistory} />
-        
-        {/* <Pie className="employeeHours" data={this.state.employeeHours} /> */}
->>>>>>> origin/chartjs
       </div>
     )
   }

96127fb012edda80543ece48d1cc970c4578ff08
danWagar <dandwagar@gmail.com>
2020-01-24 14:30:06 -0800

    catch all styling'

--- src/components/CatchAll/CatchAll.js
+++ src/components/CatchAll/CatchAll.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import skyscraper from '../../images/skyscraper.svg'
+import './CatchAll.css'
 
 export const CatchAll = () => {
   return (
-    <div>
-      <h3>Ruh Roh</h3>
-      <p>What you're looking for doesn't exist.</p>
+    <div className="LandingPage CatchAll">
+      <img src={skyscraper} alt="skyscraper" />
+      <h3 className="CatchAll__h3">Ruh Roh</h3>
+      <span className="CatchAll__span">
+        What you're looking for doesn't exist.
+      </span>
     </div>
   )
 }

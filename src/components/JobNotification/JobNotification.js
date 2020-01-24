3b553026282f20b573c2298b4cdc9ade3a716cf5
danWagar <dandwagar@gmail.com>
2020-01-23 17:42:24 -0800

    styling changes

--- src/components/JobNotification/JobNotification.js
+++ src/components/JobNotification/JobNotification.js
@@ -223,7 +223,10 @@ export default class JobNotification extends Component {
       if (this.context.currentUser.role === 'owner')
         return (
           <>
-            <button onClick={e => this.renderList(e)}>
+            <button
+              className="JobNotificaion__btn"
+              onClick={e => this.renderList(e)}
+            >
               Notifications: {this.state.notificationCount}
             </button>
             {this.state.notificationDropDown ? (
@@ -242,7 +245,10 @@ export default class JobNotification extends Component {
         )
       return (
         <>
-          <button onClick={e => this.renderList(e)}>
+          <button
+            className="JobNotificaion__btn"
+            onClick={e => this.renderList(e)}
+          >
             Notifications: {this.state.notificationCount}
           </button>
           {this.state.notificationDropDown ? (

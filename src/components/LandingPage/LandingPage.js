c9d997e3bb17dcdd01061502e1f2a16f79ea914e
danWagar <dandwagar@gmail.com>
2020-01-25 19:25:14 -0800

    add img back to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import skyscraper from '../../images/skyscraper.svg'
 import './LandingPage.css'
 
 export default function LandingPage() {
   return (
     <section className="LandingPage">
       <div className="LandingPage__hero_text">
+        <img src={skyscraper} alt="skyscraper" />
         <h1>
           <span className="LandingPage__t1">Dream Bigger,</span>
           <span className="LandingPage__t2">Build Faster,</span>

c9ad85ff748abfe4e1e1e9208cb7eaef4b1f0883
balayaydemir <balayaydemir@gmail.com>
2020-01-26 16:13:05 -0800

    demo route finished

---
+++ src/components/Demo/Demo.js
@@ -0,0 +1,133 @@
+import React, { useContext, useState } from 'react';
+import { withRouter, Redirect } from 'react-router';
+import app from '../../services/base.js';
+import { AuthContext } from '../../services/Auth.js';
+import { Label, Input } from '../Form/Form';
+import Swal from 'sweetalert2';
+import './Demo.css';
+
+const Demo = () => {
+  const { currentUser } = useContext(AuthContext);
+  const [role, setRole] = useState('worker')
+
+  const handleLogin = async event => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    app
+      .auth()
+      .signInWithEmailAndPassword(email.value, password.value)
+      .catch(error => {
+        console.warn(error);
+          Swal.fire({
+            title: 'Error!',
+            text:
+              error.code === 'auth/user-not-found'
+                ? 'Incorrect email'
+                : 'Incorrect password',
+            icon: 'error',
+            confirmButtonText: 'Close',
+          });
+      });
+  }
+
+  const getValue = (inputType) => {
+    if (role === 'worker') {
+      if (inputType === 'email') {
+        return 'jhalpert@dundermifflin.com'
+      } else {
+        return 'Password1!'
+      }
+    }
+    if (role === 'manager') {
+      if (inputType === 'email') {
+        return 'mscott@dundermifflin.com'
+      } else {
+        return 'Password1!'
+      }
+    }
+    if (role === 'owner') {
+      if (inputType === 'email') {
+        return 'dwallace@dundermifflin.com'
+      } else {
+        return 'Password1!'
+      }
+    }
+  }
+
+  const changeRole = (e, role) => {
+    e.stopPropagation();
+    setRole(role);
+  };
+
+  if (currentUser) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div className="Login">
+      <form className="Form" onSubmit={handleLogin}>
+      <h1>I want to demo as a: </h1>
+        <div className="radio-toolbar">
+          <Input
+            type="radio"
+            value="worker"
+            id="check_worker"
+            name="entry_type"
+            checked={role === 'worker'}
+            onChange={e => changeRole(e, 'worker')}
+          />
+          <Label htmlFor="check_worker">Project Worker</Label>
+          <Input
+            type="radio"
+            value="manager"
+            id="check_manager"
+            name="entry_type"
+            checked={role === 'manager'}
+            onChange={e => changeRole(e, 'manager')}
+          />
+          <Label htmlFor="check_manager">Project Manager</Label>
+          <Input
+            type="radio"
+            value="owner"
+            id="check_owner"
+            name="entry_type"
+            checked={role === 'owner'}
+            onChange={e => changeRole(e, 'owner')}
+          />
+          <Label htmlFor="check_owner">Company Owner</Label>
+        </div>
+        <Label>
+          Email
+          <Input
+            test-id="login-email"
+            name="email"
+            value={getValue('email')}
+            type="email"
+            placeholder="Email"
+            required
+          />
+        </Label>
+        <Label>
+          Password
+          <Input
+            test-id="login-password"
+            name="password"
+            value={getValue('password')}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Label>
+        <button
+          className="btn_highlight_color"
+          type="submit"
+          test-id="login-button"
+        >
+          Log in
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default withRouter(Demo);

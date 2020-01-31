3a1b09674d6b72ab08f215ed741b40b34520a176
danWagar <dandwagar@gmail.com>
2020-01-30 19:03:01 -0800

    landingPage styling changes

---
+++ build/precache-manifest.f6b33267d35d2dd6c8a0209543ded59d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31468cd0e23a2ae417768527e5098cfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64fe4870fffb8442d122",
+    "url": "/static/css/main.a7415a72.chunk.css"
+  },
+  {
+    "revision": "ddf1ad8b83f96842ba3a",
+    "url": "/static/js/2.0a78e9c6.chunk.js"
+  },
+  {
+    "revision": "f5901640c0e5426713889ad6374e99d9",
+    "url": "/static/js/2.0a78e9c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64fe4870fffb8442d122",
+    "url": "/static/js/main.43fe5fb7.chunk.js"
+  },
+  {
+    "revision": "bf779700950b52ad60d9",
+    "url": "/static/js/runtime-main.177a526c.js"
+  },
+  {
+    "revision": "25ad2c349648628874d5799e100104e6",
+    "url": "/static/media/lazy.25ad2c34.svg"
+  },
+  {
+    "revision": "ceda32987e0f68088b2c2261b4078db6",
+    "url": "/static/media/skyscraper.ceda3298.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,13 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+  "/precache-manifest.f6b33267d35d2dd6c8a0209543ded59d.js"
 );
 
-importScripts('/precache-manifest.9fa1b5a94f459f8637fe14b0f423dadc.js');
-
-self.addEventListener('message', event => {
+self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL('/index.html'),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
